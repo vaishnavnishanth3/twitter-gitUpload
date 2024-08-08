0e95c468a17b4ad3a23bcdbdf59c62132b2076e2
Vaishnav Nishanth AV <92431643+vaishnavnishanth3@users.noreply.github.com>
2024-08-08 17:43:53 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,37 @@
+import express from "express"
+import dotenv from "dotenv"
+import { v2 as cloudinary} from "cloudinary"
+import cors from "cors"
+import dbConnection from "./db/connectMongoDB.js"
+import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
+import notificationRoutes from "./routes/notification.route.js"
+import cookieParser from "cookie-parser"
+
+dotenv.config()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) //to parse form data
+app.use(cors())
+app.use(cookieParser())
+
+app.use("/api/auth",authRoutes)
+app.use("/api/users",userRoutes)
+app.use("/api/posts",postRoutes)
+app.use("/api/notifications",notificationRoutes)
+
+
+app.listen(port, () => {
+    console.log(`\nServer running on http://localhost:${port}`)
+    dbConnection()
+})
